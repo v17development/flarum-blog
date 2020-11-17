cdbe6fc172501bc36f2a33e7761e06328659f460
Jasper Vriends <4417659+jaspervriends@users.noreply.github.com>
2020-11-17 14:49:54 +0100

    Fix #7

--- js/src/forum/components/BlogCategories.js
+++ js/src/forum/components/BlogCategories.js
@@ -19,21 +19,35 @@ export default class BlogCategories extends Component {
 
           // Add tag
           tags.push(
-            <a href={app.route("blogCategory", { slug: tag.slug() })} className={`BlogCategories-item BlogCategories-item-${tag.id()}`} config={m.route}><span><i className={tag.icon()} /></span> {tag.name()}</a>
+            this.categoryItem(tag)
           );
 
           // Add tags
           app.store.all('tags').forEach(_tag => {
             if(_tag.isChild() && _tag.parent().id() === tag.id()) {
-              tags.push(
-                <a href={app.route("blogCategory", { slug: _tag.slug() })} className={`BlogCategories-item BlogCategories-item-${_tag.id()} ${app.forum.attribute('blogCategoryHierarchy') == true ? 'BlogCategories-item-child' : ''}`} config={m.route}><span><i className={_tag.icon()} /></span> {_tag.name()}</a>
-              );
+              tags.push(this.categoryItem(_tag));
             }
           });
           
           return tags;
         })}
       </div>
+    )
+  }
+
+  // Category item
+  categoryItem(tag) {
+    return (
+      <a 
+        href={app.route("blogCategory", { slug: tag.slug() })} 
+        className={`BlogCategories-item BlogCategories-item-${tag.id()} ${tag.isChild() && app.forum.attribute('blogCategoryHierarchy') == true ? 'BlogCategories-item-child' : ''}`} 
+        config={m.route}
+        >
+        <span className={tag.icon() === '' ? 'BlogCategories-item-colored' : ''} style={{ backgroundColor: tag.icon() === '' ? tag.color() : null }}>
+          <i className={tag.icon()} />
+        </span>
+        {tag.name()}
+      </a>
     )
   }
 }
