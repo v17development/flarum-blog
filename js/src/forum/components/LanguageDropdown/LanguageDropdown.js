c55b65d81836f28437979712acfb0bcb824b8ddb
Ian Morland <ian@morland.me>
2021-08-17 14:17:18 +0100

    Address language selection (#99)
    * Attempt to fix discussion-lang interop

--- js/src/forum/components/LanguageDropdown/LanguageDropdown.js
+++ js/src/forum/components/LanguageDropdown/LanguageDropdown.js
@@ -17,27 +17,30 @@ export default class LanguageDropdown extends Component {
   view() {
     const selected = this.attrs.selected;
 
-    const items = Object.keys(this.options).map((key) => {
-      const isSelected = selected || "any";
-      const active = key === isSelected;
+        return Dropdown.component(
+            {
+                buttonClassName: 'Button',
+                label: this.options[selected] || this.options[this.attrs.default],
+            },
+            Object.keys(this.options).map((key) => {
+                let defaultSelected;
+                if (app.forum.attribute('fof-discussion-language.composerLocaleDefault')) {
+                    defaultSelected = 'any';
+                } else {
+                    defaultSelected = app.translator.formatter.locale;
+                }
+                const isSelected = selected || defaultSelected;
+                const active = key === isSelected;
 
-      return Button.component(
-        {
-          active,
-          icon: active ? "fas fa-check" : true,
-          onclick: () => this.attrs.onclick(key),
-        },
-        this.options[key]
-      );
-    });
-
-    return Dropdown.component(
-      {
-        buttonClassName: "Button",
-        label: this.options[selected] || this.options[this.attrs.default],
-        menuClassName: "Dropdown-menu--right",
-      },
-      items
-    );
+                return Button.component(
+                    {
+                        active,
+                        icon: active ? 'fas fa-check' : true,
+                        onclick: () => this.attrs.onclick(key),
+                    },
+                    this.options[key]
+                );
+            })
+        );
   }
 }
