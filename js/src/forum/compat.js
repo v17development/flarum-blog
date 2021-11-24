963da9bb5e3f9d077d39910dbfb5aee679a2188d
David Wheatley <hi@davwheat.dev>
2021-11-24 21:30:46 +0000

    feat: add BlogPostController to exports

--- js/src/forum/compat.js
+++ js/src/forum/compat.js
@@ -1,6 +1,7 @@
 import { compat } from "@flarum/core/forum";
 import BlogAuthor from "./components/BlogItemSidebar/BlogAuthor";
 import BlogItemSidebar from "./components/BlogItemSidebar/BlogItemSidebar";
+import BlogPostController from "./components/BlogPostController";
 import ForumNav from "./components/ForumNav";
 import BlogItem from "./pages/BlogItem";
 import BlogOverview from "./pages/BlogOverview";
@@ -10,6 +11,7 @@ export default () => {
     "v17development/blog/components/BlogItemSidebar": BlogItemSidebar,
     "v17development/blog/components/BlogAuthor": BlogAuthor,
     "v17development/blog/components/ForumNav": ForumNav,
+    "v17development/blog/components/BlogOverview": BlogPostController,
     "v17development/blog/pages/BlogItem": BlogItem,
     "v17development/blog/pages/BlogOverview": BlogOverview,
   });
