70e0e628aca89a52f53d2ae22d80a096fb99d5e0
David Wheatley <hi@davwheat.dev>
2022-04-16 16:03:12 +0100

    feat: extract featured article to component and make extensible (#122)

--- js/src/forum/compat.js
+++ js/src/forum/compat.js
@@ -2,6 +2,7 @@ import { compat } from "@flarum/core/forum";
 import BlogAuthor from "./components/BlogItemSidebar/BlogAuthor";
 import BlogItemSidebar from "./components/BlogItemSidebar/BlogItemSidebar";
 import BlogPostController from "./components/BlogPostController";
+import FeaturedBlogItem from "./components/FeaturedBlogItem";
 import ForumNav from "./components/ForumNav";
 import BlogItem from "./pages/BlogItem";
 import BlogOverview from "./pages/BlogOverview";
@@ -12,6 +13,7 @@ export default () => {
     "v17development/blog/components/BlogAuthor": BlogAuthor,
     "v17development/blog/components/ForumNav": ForumNav,
     "v17development/blog/components/BlogPostController": BlogPostController,
+    "v17development/blog/components/FeaturedBlogItem": FeaturedBlogItem,
     "v17development/blog/pages/BlogItem": BlogItem,
     "v17development/blog/pages/BlogOverview": BlogOverview,
   });
